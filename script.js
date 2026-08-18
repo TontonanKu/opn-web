25172a07d13388a6561a9a815404c0d09afcac32
TontonanKu <langitcewe@gmail.com>
2026-08-18 16:43:09 +0700

    Swap Hiyuki and Lucila schedule

--- script.js
+++ script.js
@@ -310,9 +310,9 @@ window.addEventListener('load', () => {
         // ==========================================
         // DYNAMIC BANNER & AVATAR (Setiap 6 Jam)
         // ==========================================
-        let charName = 'lucila'; // 00:00 - 06:00 (Pagi awal)
+        let charName = 'hiyuki'; // 00:00 - 06:00 (Tengah Malam - Pagi)
         if (hours >= 6 && hours < 12) charName = 'chisa'; // 06:00 - 12:00 (Pagi - Siang)
-        else if (hours >= 12 && hours < 18) charName = 'hiyuki'; // 12:00 - 18:00 (Siang - Sore/Malam awal)
+        else if (hours >= 12 && hours < 18) charName = 'lucila'; // 12:00 - 18:00 (Siang - Sore)
         else if (hours >= 18 && hours < 24) charName = 'shorekeeper'; // 18:00 - 24:00 (Malam)
 
         const dynBanner = document.getElementById('dynamic-banner');
